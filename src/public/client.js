199f88707ee07e6a6b10dec5b0bd3cdad1704fa7
Strata Research <developers@strata.ltd>
2020-10-19 17:22:42 -0400

    feat/added event handlers to buttons to toggle rovers

--- src/public/client.js
+++ src/public/client.js
@@ -1,44 +1,49 @@
 let store = {
-  user: { name: 'Student' },
-  apod: '',
-  rovers: ['Curiosity', 'Opportunity', 'Spirit']
+  rovers: ['Curiosity', 'Opportunity', 'Spirit'],
+  selectedRover: ''
 };
+
+console.log('store', store);
 
 // add our markup to the page
 const root = document.getElementById('root');
 
 const updateStore = (store, newState) => {
-  store = Object.assign(store, newState);
+  store = { ...store, ...newState };
+  console.log('store after update:', store);
   render(root, store);
 };
 
 const render = async (root, state) => {
   root.innerHTML = App(state);
+  addEventHandlers(state);
+};
+
+const addEventHandlers = (state) => {
+  state.rovers.forEach((rover) => {
+    document.getElementById(rover).addEventListener('click', () => {
+      updateStore(store, {
+        selectedRover: rover
+      });
+    });
+  });
 };
 
 // create content
 const App = (state) => {
-  let { rovers, apod } = state;
+  const { rovers, selectedRover } = state;
 
   return `
-        <header></header>
-        <main>
-            ${Greeting(store.user.name)}
-            <section>
-                <h3>Put things on the page!</h3>
-                <p>Here is an example section.</p>
-                <p>
-                    One of the most popular websites at NASA is the Astronomy Picture of the Day. In fact, this website is one of
-                    the most popular websites across all federal agencies. It has the popular appeal of a Justin Bieber video.
-                    This endpoint structures the APOD imagery and associated metadata so that it can be repurposed for other
-                    applications. In addition, if the concept_tags parameter is set to True, then keywords derived from the image
-                    explanation are returned. These keywords could be used as auto-generated hashtags for twitter or instagram feeds;
-                    but generally help with discoverability of relevant imagery.
-                </p>
-                ${ImageOfTheDay(apod)}
-            </section>
-        </main>
-        <footer></footer>
+      <header>
+        <h1>Mars Rovers</h1>
+        <p>Choose a Mars Rover to learn more</p>
+      </header>
+      <div class="buttonContainer">
+        ${Buttons(rovers)}
+      </div>
+      <div class="roverContainerOuter">
+       ${Cards(selectedRover)}
+      </div>
     `;
 };
 
@@ -49,57 +54,32 @@ window.addEventListener('load', () => {
 
 // ------------------------------------------------------  COMPONENTS
 
-// Pure function that renders conditional information -- THIS IS JUST AN EXAMPLE, you can delete it.
-const Greeting = (name) => {
-  if (name) {
-    return `
-            <h1>Welcome, ${name}!</h1>
-        `;
-  }
+const Buttons = (array) => array.map((rover) => `<button id='${rover}'>${rover}</button>`).join('');
 
-  return `
-        <h1>Hello!</h1>
-    `;
-};
+const Cards = (selectedRover) => `<h2>${selectedRover}</h2>`;
 
-// Example of a pure function that renders information requested from the backend
-const ImageOfTheDay = (apod) => {
-  // If image does not already exist, or it is not from today -- request it again
-  const today = new Date();
-  const photodate = new Date(apod.date);
-  console.log(photodate.getDate(), today.getDate());
-
-  console.log(photodate.getDate() === today.getDate());
-  if (!apod || apod.date === today.getDate()) {
-    getImageOfTheDay(store);
-  }
-
-  const color = 'red';
-  const position = 'absolute';
-  // check if the photo of the day is actually type video!
-  if (apod.media_type === 'video') {
-    return `
-            <p>See today's featured video <a href="${apod.url}">here</a></p>
-            <p>${apod.title}</p>
-            <p>${apod.explanation}</p>
-        `;
-  } else {
-    return `
-            <img src="${apod.image.url}" height="350px" width="100%" />
-            <p style='color:${color} position:${position}>${apod.image.explanation}</p>
-        `;
-  }
-};
+// gallery
+{
+  /* <div class='roverGalleryContainer'>
+  <div class='imgContainer'>
+    <img src='https://placekeanu.com/500/300/y' alt='' />
+  </div>
+  <div class='imgContainer'>
+    <img src='https://placekeanu.com/500/300/y' alt='' />
+  </div>
+  <div class='imgContainer'>
+    <img src='https://placekeanu.com/500/300/y' alt='' />
+  </div>
+  <div class='imgContainer'>
+    <img src='https://placekeanu.com/500/300/y' alt='' />
+  </div>
+  <div class='imgContainer'>
+    <img src='https://placekeanu.com/500/300/y' alt='' />
+  </div>
+  <div class='imgContainer'>
+    <img src='https://placekeanu.com/500/300/y' alt='' />
+  </div>
+</div>; */
+}
 
 // ------------------------------------------------------  API CALLS
-
-// Example API call
-const getImageOfTheDay = (state) => {
-  let { apod } = state;
-
-  fetch(`http://localhost:3000/apod`)
-    .then((res) => res.json())
-    .then((apod) => updateStore(store, { apod }));
-
-  return data;
-};
